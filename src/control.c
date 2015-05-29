97d3000255b2c699bf0b05436f82a9ee9e8cde02
Richard Bryan <rabryan@lucidsci.com>
2015-05-29 14:11:51 -0400

    faster minute blink

--- src/control.c
+++ src/control.c
@@ -686,8 +686,8 @@ bool clock_mode_tic ( event_flags_t event_flags, uint32_t tick_cnt ) {
                   MS_IN_TICKS(1000), false);
                 phase = ANIM_MIN;
 #else
-                anim_ptr = anim_blink(min_disp_ptr, MS_IN_TICKS(400),
-                        MS_IN_TICKS(4000), false);
+                anim_ptr = anim_blink(min_disp_ptr, MS_IN_TICKS(200),
+                        MS_IN_TICKS(2000), false);
                 phase = ANIM_MIN;
 #endif
             }
