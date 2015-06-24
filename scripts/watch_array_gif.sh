0840df1e24f983a3052ad1076f742606ac70fa51
Walker Inman <winman@lucidsci.com>
2015-06-24 18:21:12 -0400

    add script for creating various interesting slides

--- scripts/watch_array_gif.sh
+++ scripts/watch_array_gif.sh
@@ -7,7 +7,7 @@ find_min_size() {
     min_width=10000
     min_height=10000
 
-    for file in $(ls *.JPG); do
+    for file in $(ls originals/*.JPG); do
         width=$(identify -format "%w" $file)
         height=$(identify -format "%h" $file)
         echo "$file is $width by $height px"
@@ -29,8 +29,8 @@ crop_images() {
     resize_width=680
     crop_dim=$(printf %dx%d+0+0 $crop_width $crop_height)
     resize_dim=$(printf %dx%d $resize_width $resize_height)
-    img_num=0
-    for file in $(ls *.JPG); do
+    img_num=$(ls | grep img_ | wc -l)
+    for file in $(ls originals/*.JPG); do
         echo $((img_num+=1)) >> /dev/null
         newname=img_$(printf %03d $img_num).jpg
         convert \( $file -gravity center -crop $crop_dim +repage \) \
@@ -38,17 +38,30 @@ crop_images() {
     done
 }
 
-rename_img() {
-    bands="navy navy navy navy "
-    bands+="br_leath br_leath br_leath br_leath br_leath "
-    bands+="gr_leath gr_leath gr_leath gr_leath "
-    bands+="pink pink pink pink "
-    bands+="orange orange orange orange "
-    bands+="red red red red "
-    bands+="black "
+reorder_images() {
+    i=0
+    for file in $(ls img*.jpg); do
+        echo $((i+=1)) >> /dev/null
+        newname=img_$(printf %03d $i).jpg
+        if [ "$file" != "$newname" ]; then
+            mv $file $newname
+        fi
+    done
+}
 
-    bkgs="tan gry red wht wht wht red tan gry gry tan red wht wht red "
-    bkgs+=" tan gry gry tan red wht wht red tan gry gry "
+rename_img() {
+    bkgs= " gry red wht wht gry red wht wht gry gry "
+    bkgs+=" tan red wht wht tan gry gry gry red wht "
+    bkgs+=" wht red tan gry gry tan red wht wht red "
+    bkgs+=" tan gry gry tan red wht wht red tan gry "
+    bkgs+=" tan red wht red gry gry tan wht red red "
+    bkgs+=" wht wht red tan gry gry red wht red gry "
+    bkgs+=" gry wht red tan tan red wht wht gry gry "
+    bkgs+=" tan red tan tan red gry gry red tan red "
+    bkgs+=" gry gry red tan tan red gry red tan tan "
+    bkgs+=" red gry gry red tan tan gry red tan tan "
+    bkgs+=" red gry wht gry tan red red tan gry wht "
+    bkgs+=" wht gry tan red red tan wht "
     i=0
     for bkg in $bkgs; do
         echo $((i+=1)) >> /dev/null
@@ -58,36 +71,54 @@ rename_img() {
     done
 }
 
+next_slide_name() {
+    slidename=slides/slide_$(printf %03d $(( $(ls slides/ | grep slide_ | tail -1 | sed 's/slide_0*\(.*\).jpg/\1/') + 1 ))).jpg
+    echo $slidename
+}
+
 join_images() {
     if [ -z "$1" ]; then
         width=3
     else
         width=$1
     fi
-
     color=$2            #tan gry wht red
-    output_slides=15
-    num_img=$(ls img_$color*.jpg | wc -l)
-    #echo $num_img
-    for slide in $( seq 1 $output_slides); do
-        slide_no=$(($(ls slides/ | grep slide_ | wc -l)+1))
-        outname=slides/slide_$(printf %03d $slide_no).jpg
+
+    outname=$(next_slide_name)
+    if [[ "$1" == *" "* ]]; then
+        imgs=$1
+    else
+        num_img=$(ls img_$color*.jpg | wc -l)
         imgs=$(shuf -i 1-$num_img -n $width)
-        ARGS="+append "
-        for img in $imgs; do
-            inname=$(ls img_$color* | head -$img | tail -1 )
-            ARGS+=" $inname "
-        done
-        ARGS+=" $outname"
-        convert $ARGS
+    fi
+
+
+    ARGS=" +append "
+    ARGS+=" ( "
+    width_pic=0
+    for img in $imgs; do
+        echo $((width_pic+=1)) >> /dev/null
+        inname=$(ls img_$color* | head -$img | tail -1 )
+        ARGS+=" $inname "
     done
+    width_px=680        # width of gif in pixels
+    im_width=$(($width_px / $width_pic))
+    final_width=$(($im_width * $width_pic))
+    ARGS+=" ) -resize $final_width "
+    ARGS+=" $outname"
+    convert $ARGS
+}
+
+next_gif_name() {
+    gifname=gifs/gif_$(printf %03d $(( $(ls gifs/ | grep gif_ | tail -1 | sed 's/gif_0*\(.*\).gif/\1/') + 1 ))).gif
+    echo $gifname
 }
 
 make_gif() {
     width_pic=3                         # number of images to use across
-    timing=$(( 150 / $width_pic ))      # x / 100 sec (ideal)
+    timing=$(( 400 / $width_pic ))      # x / 100 sec (ideal)
     gif_img_cnt=0           # number of images used in final gif (or all w/ 0)
-    loops=2
+    loops=1000
 
     if [ -n "$1" ]; then
         outname=$1
@@ -108,7 +139,7 @@ make_gif() {
         gif_img_cnt=$num_raw_img
     fi
     imgs=$(shuf -i 1-$num_raw_img -n $gif_img_cnt)
-    echo "Making a gif with these image numbers" $imgs
+    echo "Making $gifname with these image numbers" $imgs
 
     # make gif
     ARGS=" -delay $timing "
@@ -145,30 +176,54 @@ make_gif() {
     done
 
     ARGS+=" -loop $loops $outname "
-    echo "Args: $ARGS"
+    #echo "Args: $ARGS"
     convert $ARGS
 }
+
+add_time() {
+    img=img_003.jpg
+    outfile=out.jpg
+    tme="142"
+    ARGS=" ( $img "
+    #ARGS+=" -resize 680x1100> -size 680x1100 xc:white +swap \
+    #    -gravity north -composite ) -fill blue -gravity center -pointsize 100 label:$tme $outfile"
+    ARGS+=" -resize 680x1100> -size 680x1100 xc:white +swap \
+        -gravity north -composite ) \
+        -pointsize 60 -fill green \
+        -draw \"text 10,290 $tme\"  "
+    #-crop 1280x720+1200+1250 \
+    convert $img \
+        -pointsize 50 -fill black \
+        -draw "text 15,50 '1:42'" $outfile
+    #echo $ARGS
+    #convert $ARGS
+}
+
+
+mkdir -p gifs
+mkdir -p slides
 
 #find_min_size;
 #crop_images;
 #rename_img;
-#join_images 4;
+reorder_images;
+join_images "8 52 7";       # black blue
+join_images "8 67 7";       # clear blue
+join_images "1 5 12 13";    # gray bkg, brass
+join_images "3 4 7 15";     # white bkg, brass
+join_images "11 53 23 115 31"    # 16 (light pink strap) white bkg, black al
+join_images "8 114 28 51 20"     # 19 (light pink strap) gray bkg, black al
+join_images "66 76 82 109 97"      # gray bkg, clear al 87
+join_images "24 32 47 57 102"                       # black band, white bkg
+join_images "27 35 50 56 103"                       # black band, gray bkg
 #join_images 3 tan;
 #join_images 3 gry;
 #join_images 3 wht;
 #join_images 3 red;
 
-mkdir -p gifs
-mkdir -p slides
-
-gifname=gifs/gif_$(printf %03d $(( $(ls gifs | wc -l) + 1 ))).gif
-make_gif $gifname 2;
-gifname=gifs/gif_$(printf %03d $(( $(ls gifs | wc -l) + 1 ))).gif
-make_gif $gifname 3;
-gifname=gifs/gif_$(printf %03d $(( $(ls gifs | wc -l) + 1 ))).gif
-make_gif $gifname 4;
-gifname=gifs/gif_$(printf %03d $(( $(ls gifs | wc -l) + 1 ))).gif
-make_gif $gifname 5;
-if [ "$?" -eq 0 ]; then
-    eog $gifname
-fi
+#add_time;
+gifname=$(next_gif_name)
+#make_gif $gifname 5;
+#if [ "$?" -eq 0 ]; then
+#    eog $gifname
+#fi
