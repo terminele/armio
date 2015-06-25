5c65f7b52178c3b3ebe9411c73a82a757e875ec9
Walker Inman <winman@lucidsci.com>
2015-06-24 22:04:30 -0400

    cleanup watch gif scripts

--- scripts/watch_array_gif.sh
+++ scripts/watch_array_gif.sh
@@ -109,14 +109,50 @@ join_images() {
     convert $ARGS
 }
 
+add_time() {
+    img=img_003.jpg
+    outfile=out.jpg
+    tme="142"
+    ARGS=" ( $img "
+    #ARGS+=" -resize 680x1100> -size 680x1100 xc:white +swap \
+    #    -gravity north -composite ) -fill blue -gravity center -pointsize 100 label:$tme $outfile"
+    ARGS+=" -resize 680x1100> -size 680x1100 xc:white +swap \
+        -gravity north -composite ) \
+        -pointsize 60 -fill green \
+        -draw \"text 10,290 $tme\"  "
+    #-crop 1280x720+1200+1250 \
+    convert $img \
+        -pointsize 50 -fill black \
+        -draw "text 15,50 '1:42'" $outfile
+    #echo $ARGS
+    #convert $ARGS
+}
+
+make_slides() {
+    mkdir -p slides
+    rm slides/*
+    join_images "8 52 7";       # black blue
+    join_images "8 67 7";       # clear blue
+    #join_images "1 5 12 13";    # gray bkg, brass
+    join_images "3 4 7 15";     # white bkg, brass
+    join_images "11 53 23 115 31"    # 16 (light pink strap) white bkg, black al
+    #join_images "8 114 28 51 20"     # 19 (light pink strap) gray bkg, black al
+    join_images "66 76 82 109 97"      # gray bkg, clear al 87
+    join_images "24 32 47 57 102"                       # black band, white bkg
+    #join_images "27 35 50 56 103"                       # black band, gray bkg
+    #join_images 3 tan;
+    #join_images 3 gry;
+    #join_images 3 wht;
+    #join_images 3 red;
+}
+
 next_gif_name() {
-    gifname=gifs/gif_$(printf %03d $(( $(ls gifs/ | grep gif_ | tail -1 | sed 's/gif_0*\(.*\).gif/\1/') + 1 ))).gif
+    gifname=gifs/gif_$(printf %03d $(( $(ls gifs/ | grep gif_ | tail -1 | sed 's/gif_0*\(.*\).gif.*/\1/') + 1 ))).gif
     echo $gifname
 }
 
 make_gif() {
     width_pic=3                         # number of images to use across
-    timing=$(( 400 / $width_pic ))      # x / 100 sec (ideal)
     gif_img_cnt=0           # number of images used in final gif (or all w/ 0)
     loops=1000
 
@@ -129,6 +165,7 @@ make_gif() {
     if [ -n "$2" ]; then
         width_pic=$2
     fi
+    timing=$(( 300 / $width_pic ))      # x / 100 sec (ideal)
 
     width_px=680        # width of gif in pixels
     im_width=$(($width_px / $width_pic))
@@ -139,7 +176,10 @@ make_gif() {
         gif_img_cnt=$num_raw_img
     fi
     imgs=$(shuf -i 1-$num_raw_img -n $gif_img_cnt)
-    echo "Making $gifname with these image numbers" $imgs
+    txtname=$outname
+    txtname+=".txt"
+    echo "gif timing is $timing" >$txtname
+    echo "Making $gifname with these image numbers" $imgs >>$txtname
 
     # make gif
     ARGS=" -delay $timing "
@@ -159,7 +199,7 @@ make_gif() {
     for i in $(seq 1 $(( $gif_img_cnt - $width_pic ))); do
         rep_order+="$(( $(shuf -i 1-$width_pic -n 1) - 1 )) "
     done
-    echo "and this replacement order ment order" $rep_order
+    echo "and this replacement order ment order" $rep_order >>$txtname
     for i in $(seq 1 $(( $gif_img_cnt - $width_pic ))); do
         next_img=$(echo $imgs | sed 's/\([^ ]*\) \(.*\)/\1/')
         imgs=$(echo $imgs | sed 's/\([^ ]*\) \(.*\)/\2/')
@@ -180,50 +220,18 @@ make_gif() {
     convert $ARGS
 }
 
-add_time() {
-    img=img_003.jpg
-    outfile=out.jpg
-    tme="142"
-    ARGS=" ( $img "
-    #ARGS+=" -resize 680x1100> -size 680x1100 xc:white +swap \
-    #    -gravity north -composite ) -fill blue -gravity center -pointsize 100 label:$tme $outfile"
-    ARGS+=" -resize 680x1100> -size 680x1100 xc:white +swap \
-        -gravity north -composite ) \
-        -pointsize 60 -fill green \
-        -draw \"text 10,290 $tme\"  "
-    #-crop 1280x720+1200+1250 \
-    convert $img \
-        -pointsize 50 -fill black \
-        -draw "text 15,50 '1:42'" $outfile
-    #echo $ARGS
-    #convert $ARGS
-}
-
 
 mkdir -p gifs
-mkdir -p slides
 
 #find_min_size;
 #crop_images;
-#rename_img;
-reorder_images;
-join_images "8 52 7";       # black blue
-join_images "8 67 7";       # clear blue
-join_images "1 5 12 13";    # gray bkg, brass
-join_images "3 4 7 15";     # white bkg, brass
-join_images "11 53 23 115 31"    # 16 (light pink strap) white bkg, black al
-join_images "8 114 28 51 20"     # 19 (light pink strap) gray bkg, black al
-join_images "66 76 82 109 97"      # gray bkg, clear al 87
-join_images "24 32 47 57 102"                       # black band, white bkg
-join_images "27 35 50 56 103"                       # black band, gray bkg
-#join_images 3 tan;
-#join_images 3 gry;
-#join_images 3 wht;
-#join_images 3 red;
+#reorder_images;
+make_slides;
 
 #add_time;
 gifname=$(next_gif_name)
-#make_gif $gifname 5;
-#if [ "$?" -eq 0 ]; then
-#    eog $gifname
-#fi
+#echo $gifname
+make_gif $gifname 5;
+if [ "$?" -eq 0 ]; then
+    eog $gifname
+fi
