03d909d09ad6728f50bbeff4aed16c8cc9fc842e
Walker Inman <winman@lucidsci.com>
2016-02-24 20:30:47 -0500

    update python to split filter functions

--- scripts/accel_analysis.py
+++ scripts/accel_analysis.py
@@ -17,7 +17,7 @@ SAMPLE_INT_MS = 10
 rejecttime = 0
 args = None
 
-class WakeSample:
+class WakeSample( object ):
     _sample_counter = 0
     def __init__(self, xs, ys, zs, waketime = 0, confirmed = False):
         self.xs = xs
@@ -28,91 +28,88 @@ class WakeSample:
         WakeSample._sample_counter+=1
         self.i = WakeSample._sample_counter
         self._check_result = None
+        self._collect_sums()
+
+    def _collect_sums( self ):
+        self.xsums = [ sum( self.xs[:i+1] ) for i in range( len( self.xs ) ) ]
+        self.ysums = [ sum( self.ys[:i+1] ) for i in range( len( self.ys ) ) ]
+        self.zsums = [ sum( self.zs[:i+1] ) for i in range( len( self.zs ) ) ]
+
+    def _fltr_y_not_deliberate_fail( self ):
+        return self.ys[-1] < -5
+
+    def _fltr_y_turn_accept( self ):
+        assert sum( self.ys[:9] ) == self.ysums[8]
+        return abs( self.ysums[8] ) >= 240
+
+    def _fltr_x_turn_accept( self ):
+        assert sum( self.xs[:5] ) == self.xsums[4]
+        return abs(self.xsums[4]) >= 120
+
+    def _fltr_xy_turn_accept( self ):
+        return abs( self.ysums[8] ) + abs( self.xsums[4] ) > 140
+
+    def _fltr_z_sum_slope_accept( self ):
+        assert sum( self.zs[-11:] ) == sum( self.zs[:-12:-1] )
+        assert sum( self.zs[:11] ) == self.zsums[10]
+        assert sum( self.zs[:5] ) == self.zsums[4]
+        assert sum( self.zs[-11:] ) == self.zsums[31] - self.zsums[31-11]
+
+        assert self.dzN == self.zsums[31] - self.zsums[31-11] - self.zsums[10]
+
+        test1 = self.zsums[4] < 100
+        test2 = self.zsums[31] - self.zsums[31-11] - self.zsums[10] > 110
+
+        return test1 and test2
+
+    def _fltr_y_inwards_accept( self ):
+        return self.ys[-1] > 6
+
+    def _fltr_y_overshoot_accept( self ):
+        testsum1a = self.ysums[-1] - self.ysums[26]
+        testsum1b = sum( self.ys[-5:] )
+        assert( testsum1a == testsum1b )
+        test1 = testsum1a > 20
+
+        testsum2a = self.ysums[-1] - self.ysums[22]
+        testsum2b = sum(self.ys[-9:])
+        assert( testsum2a == testsum2b )
+        test2 = testsum2a > 40
+        return test1 or test2
 
     def wakeup_check(self):
         if self._check_result is not None:
             return self._check_result
 
+        self.dzN = sum( self.zs[-11:] ) - sum( self.zs[:11] )
+        self.ysum_n = self.ysums[8]
+        self.xsum_n = self.xsums[4]
 
-        wakeup = False
-        XSUM_N = 5
-        YSUM_N = 9
-        ZSUM_N = 11
-        YSUM_THS = 240
-        XSUM_THS = 120
-        XYSUM_THS = 140
-        DZ_THS = 110
-        XZ_SUM_THS = 240
-        YZ_SUM_THS = 360
-        Y_LATEST_THS = 6
-        zsum_last_n = sum(self.zs[:-ZSUM_N-1:-1])
-        zsum_first_n = sum(self.zs[:ZSUM_N])
-        self.ysum_n = ysum_n = sum(self.ys[:YSUM_N])
-        self.xsum_n = xsum_n = sum(self.xs[:XSUM_N])
-        self.dzN = dzN = zsum_last_n - zsum_first_n
-        
-        #if abs(ysum_n) + abs(xsum_n) >= 140: 
-        #    return True
-        #else:
-        #    return False
+        if False:
+            pass
+        elif self._fltr_y_turn_accept():
+            self._check_result = True
+        elif self._fltr_y_not_deliberate_fail():
+            self._check_result = False
+        elif self._fltr_xy_turn_accept():
+            self._check_result = True
+        elif self._fltr_x_turn_accept():
+            self._check_result = True
+        elif self._fltr_z_sum_slope_accept():
+            self._check_result = True
+        elif self._fltr_y_inwards_accept():
+            self._check_result = True
+        elif self._fltr_y_overshoot_accept():
+            self._check_result = True
+        else:
+            self._check_result = False
 
-        #if dzN > DZ_THS:
-        #    log.info("{}: passes dzN".format(self.i))
-        #    wakeup = True
-        
+        log.info("{}: {} rejected with dzN={}  XSUM_N={}  YSUM_N={}  Y_LAST={}".format(
+            self.i, "correctly" if not self._check_result and not self.confirmed else "falsely",
+            self.dzN, self.xsum_n, self.ysum_n, self.ys[-1]))
 
-        if sum(self.zs[:5]) < 100 and dzN > DZ_THS:
-            log.info("{}: passes dzN".format(self.i))
-            wakeup = True
+        return self._check_result
 
-        if abs(ysum_n) >= YSUM_THS:
-            log.info("{}: passes YSUM_N THS".format(self.i))
-            wakeup = True
-        
-        if abs(xsum_n) >= XSUM_THS:
-            log.info("{}: passes XSUM_N THS".format(self.i))
-            wakeup = True
-        
-        if self.ys[-1] >= Y_LATEST_THS:
-            log.info("{}: passes Y_LAST THS".format(self.i))
-            wakeup = True
-        
-        if sum(self.ys[:-6:-1]) > 20: #overshoot towards body
-            wakeup = True
-        
-        if sum(self.ys[:-10:-1]) > 40: #overshoot towards body
-            wakeup = True
-    
-        if abs(ysum_n) + abs(xsum_n) > XYSUM_THS:
-            wakeup = True
-        
-        if self.ys[-1] < -5 and abs(ysum_n) < YSUM_THS:
-            wakeup = False
-        
-#        if abs(ysum_n) + abs(dzN) > YZ_SUM_THS:
-#            wakeup = True
-#
-#        if abs(xsum_n) + abs(dzN) > XZ_SUM_THS:
-#            wakeup = True
-
-        #if abs(ysum_n) + dzN > 325:
-        #    wakeup = True
-        
-        #if abs(xsum_n) + dzN > 225:
-        #    wakeup = True
-        
-        
-        if not wakeup and not self.confirmed:
-            log.info("{}: correctly rejected with dzN={}  XSUM_N={}  YSUM_N={}  Y_LAST={}".format(
-                self.i, dzN, xsum_n, ysum_n, self.ys[-1]))
-        elif not wakeup and self.confirmed:
-            log.error("{}: FALSELY rejected with dzN={}  XSUM_N={}  YSUM_N={}  Y_LAST={}".format(
-                self.i, dzN, xsum_n, ysum_n, self.ys[-1]))
-         
-        self._check_result = wakeup
-
-        return wakeup
-    
     def is_false_negative(self):
         return not self.wakeup_check() and self.confirmed
 
@@ -123,9 +120,9 @@ class WakeSample:
         dxN = sum(self.xs[:n:-1]) - xN
         dyN = sum(self.ys[:n:-1]) - yN
         dzN = sum(self.zs[:n:-1]) - zN
-        
+
         #return abs(xN) + abs(yN) + abs(dxN) + abs(dyN) + dzN
-        
+
         if self.ys[-1] < -10:
             return 0
 
@@ -142,7 +139,7 @@ def parse_fifo(fname):
     except:
         log.error ("Unable to open file \'{}\'".format(fname))
         return []
-    
+
     binval = f.read(4)
 
     """ find first start delimiter"""
@@ -166,38 +163,38 @@ def parse_fifo(fname):
 
 
     samples = []
-    
+
     """ loop over binary file in chunks of 6 bytes
         and parse out:
             FIFO Start Code - 0xaaaaaaaa
             FIFO Data - 32x3x2-bytes
             FIFO End Code - 0xeeeeeeeee
-            (optional) waketime log code 0xddee followed by waketime ticks as unsigned int 
+            (optional) waketime log code 0xddee followed by waketime ticks as unsigned int
     """
     i = 0
     binval = f.read(6)
     while binval:
         if len(binval) != 6:
             log.info("end of file encountered")
-            break 
+            break
         data = struct.unpack("<IBB", binval)
         end_unconfirm = (data[0] == 0xeeeeeeee)#FIFO end code
         end_confirm = (data[0] == 0xcccccccc) #FIFO end code
         if end_confirm or end_unconfirm:
             """end of fifo data"""
             log.debug("Found fifo end at 0x{:08x}".format(f.tell()))
-            
+
             if data[1:] == (0xdd, 0xee): #waketime log code
                 binval = f.read(4)
                 waketime_ticks = struct.unpack("<I", binval)[0]
-                waketime_ms = waketime_ticks/TICKS_PER_MS 
+                waketime_ms = waketime_ticks/TICKS_PER_MS
                 binval = f.read(6) # update binval with next 6 bytes
             else:
                 """ waketime not include in this log dump"""
                 waketime_ms = 0
                 binval = binval[-2:] #retain last 2 bytes
                 binval += f.read(4)
-            
+
             samples.append(WakeSample(xs,ys,zs, waketime_ms, end_confirm))
             i+=1
             log.debug("new sample: waketime={}ms confirmed={}".format(waketime_ms,
@@ -252,7 +249,7 @@ def parse_fifo(fname):
         if struct.unpack("<Ibb", binval)[0] == 0xffffffff:
             log.debug("No more data after 0x{:08x}".format(f.tell()))
             break
-    
+
     log.info("Parsed {} samples from {}".format(len(samples), fname))
 
     return samples
@@ -269,7 +266,7 @@ def plot_sumN_scores(samples):
             else: unconf_scores_n.append(score)
         conf_scores.append(conf_scores_n)
         unconf_scores.append(unconf_scores_n)
-    
+
     for n in range(5, 20):
         conf = conf_scores[n-5]
         plt.plot(list(2*n-0.2 - 0.4*random.random() for i in range(len(conf))), conf, 'b.', label='confirmed')
@@ -280,7 +277,7 @@ def plot_sums(samples, x_cnt = 5, y_cnt = 8):
     xsums = [sum(s.xs[:x_cnt]) for s in samples if s.confirmed]
     ysums = [sum(s.ys[:y_cnt]) for s in samples if s.confirmed]
     ssums = [abs(x) + abs(y) for x,y in zip(xsums, ysums)]
-    
+
     xsums_u = [sum(s.xs[:x_cnt]) for s in samples if not s.confirmed]
     ysums_u = [sum(s.ys[:y_cnt]) for s in samples if not s.confirmed]
     ssums_u = [abs(x) + abs(y) for x,y in zip(xsums_u, ysums_u)]
@@ -288,19 +285,19 @@ def plot_sums(samples, x_cnt = 5, y_cnt = 8):
     #plt.plot(xsums, color="r")
     plt.plot(ysums, marker=".", color="b")
     #plt.plot(ssums, color="g")
-    
+
     #plt.plot(xsums_u, linestyle=":", color="r")
     plt.plot(ysums_u, linestyle=":",  color="b")
     #plt.plot(ssums_u, linestyle=":",  color="g")
 
 def analyze_samples(samples):
     log.info("Analyzing {} FIFO Samples".format(len(samples)))
-    
+
     wakeups = 0
     rejects = 0
     false_negatives = 0
     confirmed_wakeup_passes = 0
-    
+
     sigma_zs = []
     sigma_zs_rev = []
     sigma_ys = []
@@ -324,7 +321,7 @@ def analyze_samples(samples):
         xsum_n = sum(xs[:5])
         delta_z_12 = sum(zs[:-13:-1]) - sum(zs[:12])
         wakeup_check_pass = sample.wakeup_check()
-        if wakeup_check_pass: 
+        if wakeup_check_pass:
             wakeups+=1
             if confirmed: confirmed_wakeup_passes +=1
         else:
@@ -332,8 +329,8 @@ def analyze_samples(samples):
             global rejecttime
             rejecttime+=waketime
             if confirmed: false_negatives+=1
-        
-        
+
+
         if args.plot_all or (wakeup_check_pass and args.plot_passes) or \
                 (not wakeup_check_pass and args.plot_rejects) or \
                 (args.plot_false_negatives and sample.is_false_negative()):
@@ -341,7 +338,7 @@ def analyze_samples(samples):
             ax = fig.add_subplot(111)
             log.debug("plotting {} with xsum_n {}: ysum_n: {}  zsum_n: {} dy_10: {} dz12: {}".format(uid,
                 xsum_n, ysum_n, zsum_n, sum(ys[:-11:-1]), delta_z_12))
-            plt.title("sample {}: pass={} confirmed={} xN={} yN={} dzN={}".format(i, 
+            plt.title("sample {}: pass={} confirmed={} xN={} yN={} dzN={}".format(i,
                 wakeup_check_pass, confirmed, sample.xsum_n, sample.ysum_n, sample.dzN))
             ax.grid()
             t = range(len(xs))
@@ -357,6 +354,9 @@ def analyze_samples(samples):
             ax.set_xlim([0, len(xs)])
             ax.set_ylim([-60, 60])
 
+            plt.xlabel( "1 / Output Data Rate" )
+            plt.ylabel( "1/32 * g's for +/-4g" )
+
             plt.show()
 
         sigma_zs.append(np.cumsum(zs))
@@ -370,7 +370,7 @@ def analyze_samples(samples):
                 writer = csv.writer(csvfile, delimiter=' ')
                 for i, (x, y, z) in enumerate(zip(xs, ys, zs)):
                     writer.writerow([i, x, y, z])
-    
+
     log.info("{} wakeups ({} %)".format(wakeups, 100.0*wakeups/len(samples)))
     log.info("{} rejects ({} %)".format(rejects, 100.0*rejects/len(samples)))
     total_waketime = sum([s.waketime for s in samples])
@@ -383,13 +383,13 @@ def analyze_samples(samples):
 
     log.info("{:.2f}s total reject-saved waketime ({:.1f}%)".format(
         rejecttime/1000.0, 100*rejecttime/total_waketime))
-    
+
     total_confirms = confirmed_wakeup_passes + false_negatives
-    log.info("{} confirmed wakeup passes ({} %)".format(confirmed_wakeup_passes, 
+    log.info("{} confirmed wakeup passes ({} %)".format(confirmed_wakeup_passes,
         100*confirmed_wakeup_passes/total_confirms if total_confirms > 0 else 0))
-    log.info("{} false_negatives ({} %)".format(false_negatives, 
+    log.info("{} false_negatives ({} %)".format(false_negatives,
         100*false_negatives/total_confirms if total_confirms > 0 else 0))
-     
+
     if args.plot_csums:
         fig = plt.figure()
         plt.title("Sigma x-values")
@@ -466,7 +466,7 @@ def analyze_samples(samples):
 
         plt.scatter(xs, dzs)
         plt.show()
-    
+
     return samples
 
 def analyze_streamed(f):
@@ -475,7 +475,7 @@ def analyze_streamed(f):
     #skip any leading 0xffffff bytes
     while struct.unpack("<I", binval)[0] == 0xffffffff:
         binval = f.read(4)
-    
+
     t_offset = 0
     ts = []
     xs = []
@@ -487,9 +487,9 @@ def analyze_streamed(f):
         (z,x,t) = struct.unpack("<bbH", binval)
         t+=t_offset
         if len(ts) > 1 and t < ts[-1]:
-            t += 2**16 - 1 
-            t_offset += 2**16 - 1 
-        
+            t += 2**16 - 1
+            t_offset += 2**16 - 1
+
         ts.append(t)
         xs.append(x)
         zs.append(z)
@@ -519,10 +519,10 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--plot_passes', action='store_true', default=False)
     parser.add_argument('-c', '--plot_csums', action='store_true', default=False)
     parser.add_argument('-fn', '--plot_false_negatives', action='store_true', default=False)
-    
+
     args = parser.parse_args()
     write_csv = args.write_csv
-    
+
     if not args.streamed and args.fifo:
         samples = []
         for fname in args.dumpfiles:
